38a4382231ceb7fd7bbd2f73c01f83615d068f02
Santiago Guerra <31255580+GuerraSa@users.noreply.github.com>
2025-12-10 19:40:59 -0800

    Update dictionary names

--- data_urls.py
+++ data_urls.py
@@ -1,4 +1,9 @@
 whistler_data_url = {
     "Lifts" : "https://whistlerpeak.com/elevations/",
     "Snowfall History" : "https://whistlerpeak.com/snow/history/",
+    "Weather Forecast" : {
+        "1400m" : "https://whistlerpeak.com/weather/",
+        "1800m" : "https://whistlerpeak.com/forecast/",
+        "2248m" : "https://whistlerpeak.com/weather-top/"
+    }
 }
