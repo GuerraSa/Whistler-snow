b11d94898bc0ae98af892c9eeefe5e817d67d08a
Santiago Guerra <31255580+GuerraSa@users.noreply.github.com>
2025-12-16 14:52:18 -0600

    Added Weather Station URLs

--- data_urls.py
+++ data_urls.py
@@ -5,5 +5,19 @@ whistler_data_url = {
         "1400m" : "https://whistlerpeak.com/weather/",
         "1800m" : "https://whistlerpeak.com/forecast/",
         "2248m" : "https://whistlerpeak.com/weather-top/"
+    },
+    "Weather Stations": {
+        "7th Heaven" : "https://whistlerpeak.com/temps/seventh/",
+        "Peak" : "https://whistlerpeak.com/temps/peak/",
+        "Symphony" : "https://whistlerpeak.com/temps/symphony/",
+        "Harmony" : "https://whistlerpeak.com/temps/harmony/",
+        "Rendezvous" : "https://whistlerpeak.com/temps/rendezvous/",
+        "Roundhouse" : "https://whistlerpeak.com/temps/roundhouse/",
+        "Crystal" : "https://whistlerpeak.com/temps/crystal/",
+        "Pig Alley" : "https://whistlerpeak.com/temps/pigalley/",
+        "Catskinner" : "https://whistlerpeak.com/temps/catskinner/",
+        "Olympic" : "https://whistlerpeak.com/temps/olympic/",
+        "Village EC" : "https://whistlerpeak.com/temps/valley/",
+        "Base" : "https://whistlerpeak.com/temps/base/"
     }
 }
