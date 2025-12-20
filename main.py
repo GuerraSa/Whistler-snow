b49e7600bd82b019155885bd07293bd4966ff614
Santiago Guerra <31255580+GuerraSa@users.noreply.github.com>
2025-12-20 13:15:34 -0600

    Updated to remove snowfall history scraper to add to utils.forecast. Simplified main function

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import utils.forecast as forecast
 import utils.func as func
 
-
+# --- EXECUTION ---
+# You can now run all four:
 forecast.get_forecast("1480m")
 forecast.get_forecast("1800m")
 forecast.get_forecast("2248m")
